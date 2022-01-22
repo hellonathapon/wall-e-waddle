c5a4cf1e5fd43d8cbcaf392b7e931d0a547a3321
hellonathapon <hellonathapon@gmail.com>
2022-01-22 09:19:28 +0700

    Move x, y and keep track of direction on parameters commands

--- main.py
+++ main.py
@@ -1,10 +1,43 @@
 import re
 
-def foo(str):
+def walking(str):
+    x = 0
+    y = 0
+    facing_dir = 0
+    directions = ['North', 'South', 'East', 'West']
     cmd_str = re.sub(r'([W|R|L])(?!$)', r',\1', str) # regex = insert comma in front of W|R|L chars.
     cmd_lst = cmd_str.split(",")                     # split each commands into list
     cmd_lst.pop(0)                                   # pop out the fist index which is an extra comma
     
-    return cmd_lst
+    for cmd in cmd_lst:
+        if 'W' in cmd:
+            n = int(cmd[1:])    # slicing W char and parse the rest to int
+            if facing_dir == 0: # moves either x or y accordingly to its coordinate planes
+                y += n
+            elif facing_dir == 1:
+                x += n
+            elif facing_dir == 2:
+                y -= n
+            elif facing_dir == 3:
+                x -= n
+        elif cmd == 'R':
+            if facing_dir == 3:
+                facing_dir = 0
+            elif facing_dir == 0:
+                facing_dir += 1
+            elif facing_dir == 1:
+                facing_dir += 1
+            else:
+                facing_dir += 1
+        elif cmd == 'L':
+            if facing_dir == 0:
+                facing_dir = 3
+            elif facing_dir == 3:
+                facing_dir -= 1
+            elif facing_dir == 2:
+                facing_dir -= 1
+            else:
+                facing_dir -= 1
+    return 'X: {} Y: {} Direction: {}'.format(x,y,directions[facing_dir])
 
-print(foo("W3RW2LLRRLRLRW34W67"))
+print(walking("W5RW7RRW12LW2"))
